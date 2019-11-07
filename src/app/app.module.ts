ec00ac01f6ce6e1eedb7ca06cc07655b52f247c5
anaghak07 <anagha.kulkarni@metamagic.in>
2019-11-07 09:44:19 +0530

    adding button and email input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+
+import { AmexioEmailInputComponent } from './emailinput/emailinput.component';
+import { AmexioButtonComponent } from './buttons/button.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { AmexioWidgetModule,
+  AmexioChartsModule,
+  AmexioDashBoardModule,
+  AmexioEnterpriseModule,
+  AmexioMapModule,
+  AmexioFormsModule } from "amexio-ng-extensions"; // Import Amexio library
+import { AmexioInputHelperComponent } from './base/input.helper.component';
+import { CommonIconComponent } from './base/components/common.icon.component';
+import { CommonDataService } from './services/common.data.service';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AmexioButtonComponent,
+    AmexioEmailInputComponent,
+    AmexioInputHelperComponent,
+    CommonIconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmexioWidgetModule,
+    FormsModule,
+    AmexioChartsModule,
+    AmexioDashBoardModule,
+    AmexioEnterpriseModule,
+    AmexioMapModule,
+    AmexioFormsModule
   ],
-  providers: [],
+  providers: [CommonDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/buttons/button.component.spec.ts
@@ -0,0 +1,49 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { IconLoaderService } from '../services/icon.service';
+import { CommonIconComponent } from '../base/components/common.icon.component';
+import { AmexioButtonComponent } from './button.component';
+
+describe('amexio-button', () => {
+  let comp: AmexioButtonComponent;
+  let fixture: ComponentFixture<AmexioButtonComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [AmexioButtonComponent, CommonIconComponent],
+      providers: [IconLoaderService],
+    });
+    fixture = TestBed.createComponent(AmexioButtonComponent);
+    comp = fixture.componentInstance;
+    event = jasmine.createSpyObj('event', ['preventDefault', 'stopPropagation']);
+  });
+
+  it('buttonClick(): ', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    comp.ispressed = true;
+    comp.disabled = true;
+    fixture.detectChanges();
+    expect(comp.ispressed).toEqual(true);
+    if(comp.disabled)
+    {
+      fixture.detectChanges();
+      expect(comp.disabled).toEqual(true);
+      button.triggerEventHandler('click', {});
+    }
+  //   expect(
+  //   fixture.debugElement.query(By.css('h1')).nativeElement.innerText
+  // ).toEqual('😜');
+  });
+
+
+  it('buttonClick()', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    comp.ispressed = false;
+    comp.disabled = false;
+    fixture.detectChanges();
+    expect(comp.ispressed).toEqual(false);
+    expect(comp.disabled).toEqual(false);
+  });  
+});
