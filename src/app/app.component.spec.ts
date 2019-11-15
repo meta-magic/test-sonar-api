b137577a7fb7d47ddfcbc20d8b8e3033cd9709b8
anaghak07 <anagha.kulkarni@metamagic.in>
2019-11-15 16:40:53 +0530

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('test-api-amexio');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-api-amexio app is running!');
-  });
 });
